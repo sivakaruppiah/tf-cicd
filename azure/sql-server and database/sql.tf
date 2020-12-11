107346f650391bf7ef8456b6b791a8ce18aa60be
Vamsi <terraformingcloud@gmail.com>
2020-12-11 08:57:12 +0530

    Updated the sql server and db config

--- azure/sql-server and database/sql.tf
+++ azure/sql-server and database/sql.tf
@@ -7,11 +7,11 @@
 #
 
 provider "azurerm" {
-    version         =   ">= 2.25.0"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
+    // version         =   ">= 2.25.0"
+    // client_id       =   var.client_id
+    // client_secret   =   var.client_secret
+    // subscription_id =   var.subscription_id
+    // tenant_id       =   var.tenant_id
     
     features {}
 }
--- azure/sql-server and database/variables.tf
+++ azure/sql-server and database/variables.tf
@@ -6,27 +6,27 @@
 #- SPN Variables
 #
 
-# Service Principal Variables
+// # Service Principal Variables
 
-variable "client_id" {
-    description =   "Client ID (APP ID) of the application"
-    type        =   string
-}
+// variable "client_id" {
+//     description =   "Client ID (APP ID) of the application"
+//     type        =   string
+// }
 
-variable "client_secret" {
-    description =   "Client Secret (Password) of the application"
-    type        =   string
-}
+// variable "client_secret" {
+//     description =   "Client Secret (Password) of the application"
+//     type        =   string
+// }
 
-variable "subscription_id" {
-    description =   "Subscription ID"
-    type        =   string
-}
+// variable "subscription_id" {
+//     description =   "Subscription ID"
+//     type        =   string
+// }
 
-variable "tenant_id" {
-    description =   "Tenant ID"
-    type        =   string
-}
+// variable "tenant_id" {
+//     description =   "Tenant ID"
+//     type        =   string
+// }
 
 
 #
